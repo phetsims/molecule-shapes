7c0b0111095b2b4448f9dd06e9fe8689db78c657
Jonathan Olson <olsonsjc@gmail.com>
2014-10-03 09:30:50 -0600

    Real/model switch on 2nd screen

--- js/screens/real/RealMoleculesScreenView.js
+++ js/screens/real/RealMoleculesScreenView.js
@@ -10,12 +10,18 @@ define( function( require ) {
 
   // modules
   var inherit = require( 'PHET_CORE/inherit' );
+  var AquaRadioButton = require( 'SUN/AquaRadioButton' );
+  var Text = require( 'SCENERY/nodes/Text' );
+  var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var MoleculeShapesScreenView = require( 'MOLECULE_SHAPES/view/MoleculeShapesScreenView' );
   var MoleculeShapesPanel = require( 'MOLECULE_SHAPES/view/MoleculeShapesPanel' );
+  var MoleculeShapesColors = require( 'MOLECULE_SHAPES/view/MoleculeShapesColors' );
   var OptionsNode = require( 'MOLECULE_SHAPES/view/OptionsNode' );
   var MoleculeView = require( 'MOLECULE_SHAPES/view/3d/MoleculeView' );
 
   var optionsString = require( 'string!MOLECULE_SHAPES/control.options' );
+  var realViewString = require( 'string!MOLECULE_SHAPES/control.realView' );
+  var modelViewString = require( 'string!MOLECULE_SHAPES/control.modelView' );
 
   /**
    * Constructor for the RealMoleculesScreenView
@@ -32,6 +38,34 @@ define( function( require ) {
       right: this.layoutBounds.right - 10,
       top: this.layoutBounds.top + 10
     } ) );
+
+
+    if ( !model.isBasicsVersion ) {
+      // we offset the camera, so we don't have an exact constant. this is tuned
+      var approximateVisualCenterX = this.layoutBounds.width / 2 - 100;
+
+      // NOTE: these font sizes are scaled!
+      var realViewLabel = new Text( realViewString, { font: new PhetFont( 28 ) } );
+      var modelViewLabel = new Text( modelViewString, { font: new PhetFont( 28 ) } );
+      MoleculeShapesColors.link( 'controlPanelText', function( color ) {
+        realViewLabel.fill = modelViewLabel.fill = color;
+      } );
+
+      var horizontalSpacing = 30;
+
+      var realRadioButton = new AquaRadioButton( model.showRealViewProperty, true, realViewLabel, {
+        scale: 0.7,
+        top: this.layoutBounds.top + 10,
+        right: approximateVisualCenterX - horizontalSpacing / 2
+      } );
+      var modelRadioButton = new AquaRadioButton( model.showRealViewProperty, false, modelViewLabel, {
+        scale: 0.7,
+        top: this.layoutBounds.top + 10,
+        left: approximateVisualCenterX + horizontalSpacing / 2
+      } );
+      this.addChild( realRadioButton );
+      this.addChild( modelRadioButton );
+    }
   }
 
   return inherit( MoleculeShapesScreenView, RealMoleculesScreenView );
