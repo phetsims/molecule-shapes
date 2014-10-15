f9cb97de5d9735346c10c62bc3f39a3bdbe30960
Jonathan Olson <olsonsjc@gmail.com>
2014-10-15 04:00:18 -0600

    FastMath docs and cleanup

--- js/model/FastMath.js
+++ js/model/FastMath.js
@@ -2,6 +2,7 @@
 
 /**
  * Fast (mostly 3x3 matrix) computations at the lower level needed for Molecule Shapes' model.
+ * Overall, it uses a heavily mutable style, passing in the object where the result(s) will be stored.
  *
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
@@ -9,17 +10,21 @@ define( function( require ) {
   'use strict';
 
   /*
-   * Matrices are stored with row-major indices. For example, for a 3x3:
+   * Matrices are stored as flat typed arrays with row-major indices. For example, for a 3x3:
    * [0] [1] [2]
    * [3] [4] [5]
    * [6] [7] [8]
+   *
+   * NOTE: We assume the typed arrays are AT LEAST as long as necessary (but could be longer). This allows us to use
+   * an array as big as the largest one we'll need.
    */
 
-   var FastMathArray = window.Float64Array || Array;
+   // constants
+   var SQRT_HALF = Math.sqrt( 0.5 );
+   var FastMathArray = window.Float64Array || Array; // typed arrays give significant speed-ups
 
    // a matrix used in FastMath SVD computations
    var scratchGivens = new FastMathArray( 9 );
-   var SQRT_HALF = Math.sqrt( 0.5 );
 
    return {
     Array: FastMathArray,
@@ -28,12 +33,27 @@ define( function( require ) {
     * 3x3 matrix math
     *----------------------------------------------------------------------------*/
 
-    // 0-indexed rows and columns
-    index3: function( row, column ) {
-      return 3 * row + column;
+    /*
+     * From 0-indexed row and column indices, returns the index into the flat array
+     *
+     * @param {number} row
+     * @param {number} col
+     */
+    index3: function( row, col ) {
+      assert && assert( row >= 0 && row < 3 );
+      assert && assert( col >= 0 && col < 3 );
+      return 3 * row + col;
     },
 
+    /*
+     * Copies one matrix into another
+     *
+     * @param {FastMath.Array} matrix - [input] 3x3 Matrix
+     * @param {FastMath.Array} result - [output] 3x3 Matrix
+     */
     set3: function( matrix, result ) {
+      assert && assert( matrix.length >= 9 );
+      assert && assert( result.length >= 9 );
       result[0] = matrix[0];
       result[1] = matrix[1];
       result[2] = matrix[2];
@@ -45,8 +65,15 @@ define( function( require ) {
       result[8] = matrix[8];
     },
 
-    // transpose( matrix )
+    /*
+     * Writes the transpose of the input matrix into the result matrix (in-place modification is OK)
+     *
+     * @param {FastMath.Array} matrix - [input] 3x3 Matrix
+     * @param {FastMath.Array} result - [output] 3x3 Matrix
+     */
     transpose3: function( matrix, result ) {
+      assert && assert( matrix.length >= 9 );
+      assert && assert( result.length >= 9 );
       var m1 = matrix[3];
       var m2 = matrix[6];
       var m3 = matrix[1];
@@ -64,14 +91,30 @@ define( function( require ) {
       result[8] = matrix[8];
     },
 
+    /*
+     * The determinant of a 3x3 matrix
+     *
+     * @param {FastMath.Array} matrix - [input] 3x3 Matrix
+     * @returns {number} - The determinant. 0 indicates a singular (non-invertible) matrix.
+     */
     det3: function( matrix ) {
+      assert && assert( matrix.length >= 9 );
       return matrix[0] * matrix[4] * matrix[8] + matrix[1] * matrix[5] * matrix[6] +
              matrix[2] * matrix[3] * matrix[7] - matrix[2] * matrix[4] * matrix[6] -
              matrix[1] * matrix[3] * matrix[8] - matrix[0] * matrix[5] * matrix[7];
     },
 
-    // left * right
+    /*
+     * Writes the matrix multiplication ( left * right ) into result. (in-place modification is OK)
+     *
+     * @param {FastMath.Array} left - [input] 3x3 Matrix
+     * @param {FastMath.Array} right - [input] 3x3 Matrix
+     * @param {FastMath.Array} result - [output] 3x3 Matrix
+     */
     mult3: function( left, right, result ) {
+      assert && assert( left.length >= 9 );
+      assert && assert( right.length >= 9 );
+      assert && assert( result.length >= 9 );
       var m0 = left[0] * right[0] + left[1] * right[3] + left[2] * right[6];
       var m1 = left[0] * right[1] + left[1] * right[4] + left[2] * right[7];
       var m2 = left[0] * right[2] + left[1] * right[5] + left[2] * right[8];
@@ -92,8 +135,17 @@ define( function( require ) {
       result[8] = m8;
     },
 
-    // transpose( left ) * right
+    /*
+     * Writes the matrix multiplication ( transpose( left ) * right ) into result. (in-place modification is OK)
+     *
+     * @param {FastMath.Array} left - [input] 3x3 Matrix
+     * @param {FastMath.Array} right - [input] 3x3 Matrix
+     * @param {FastMath.Array} result - [output] 3x3 Matrix
+     */
     mult3LeftTranspose: function( left, right, result ) {
+      assert && assert( left.length >= 9 );
+      assert && assert( right.length >= 9 );
+      assert && assert( result.length >= 9 );
       var m0 = left[0] * right[0] + left[3] * right[3] + left[6] * right[6];
       var m1 = left[0] * right[1] + left[3] * right[4] + left[6] * right[7];
       var m2 = left[0] * right[2] + left[3] * right[5] + left[6] * right[8];
@@ -114,8 +166,17 @@ define( function( require ) {
       result[8] = m8;
     },
 
-    //  left * transpose( right )
+    /*
+     * Writes the matrix multiplication ( left * transpose( right ) ) into result. (in-place modification is OK)
+     *
+     * @param {FastMath.Array} left - [input] 3x3 Matrix
+     * @param {FastMath.Array} right - [input] 3x3 Matrix
+     * @param {FastMath.Array} result - [output] 3x3 Matrix
+     */
     mult3RightTranspose: function( left, right, result ) {
+      assert && assert( left.length >= 9 );
+      assert && assert( right.length >= 9 );
+      assert && assert( result.length >= 9 );
       var m0 = left[0] * right[0] + left[1] * right[1] + left[2] * right[2];
       var m1 = left[0] * right[3] + left[1] * right[4] + left[2] * right[5];
       var m2 = left[0] * right[6] + left[1] * right[7] + left[2] * right[8];
@@ -136,8 +197,19 @@ define( function( require ) {
       result[8] = m8;
     },
 
-    //  transpose( left ) * transpose( right ) === transpose( right * left )
+    /*
+     * Writes the matrix multiplication ( transpose( left ) * transpose( right ) ) into result.
+     * (in-place modification is OK)
+     * NOTE: This is equivalent to transpose( right * left ).
+     *
+     * @param {FastMath.Array} left - [input] 3x3 Matrix
+     * @param {FastMath.Array} right - [input] 3x3 Matrix
+     * @param {FastMath.Array} result - [output] 3x3 Matrix
+     */
     mult3BothTranspose: function( left, right, result ) {
+      assert && assert( left.length >= 9 );
+      assert && assert( right.length >= 9 );
+      assert && assert( result.length >= 9 );
       var m0 = left[0] * right[0] + left[3] * right[1] + left[6] * right[2];
       var m1 = left[0] * right[3] + left[3] * right[4] + left[6] * right[5];
       var m2 = left[0] * right[6] + left[3] * right[7] + left[6] * right[8];
@@ -158,8 +230,15 @@ define( function( require ) {
       result[8] = m8;
     },
 
-    // vector3 => vector3
+    /*
+     * Writes the product ( matrix * vector ) into result. (in-place modification is OK)
+     *
+     * @param {FastMath.Array} matrix - [input] 3x3 Matrix
+     * @param {Vector3} vector - [input]
+     * @param {Vector3} result - [output]
+     */
     mult3Vector3: function( matrix, vector, result ) {
+      assert && assert( matrix.length >= 9 );
       var x = matrix[0] * vector.x + matrix[1] * vector.y + matrix[2] * vector.z;
       var y = matrix[3] * vector.x + matrix[4] * vector.y + matrix[5] * vector.z;
       var z = matrix[6] * vector.x + matrix[7] * vector.y + matrix[8] * vector.z;
@@ -168,8 +247,15 @@ define( function( require ) {
       result.z = z;
     },
 
-    // in-place
+    /*
+     * Swaps two columns in a matrix, negating one of them to maintain the sign of the determinant.
+     *
+     * @param {FastMath.Array} matrix - [input] 3x3 Matrix
+     * @param {number} idx0 - In the range [0,2]
+     * @param {number} idx1 - In the range [0,2]
+     */
     swapNegateColumn: function( matrix, idx0, idx1 ) {
+      assert && assert( matrix.length >= 9 );
       var tmp0 = matrix[idx0];
       var tmp1 = matrix[idx0 + 3];
       var tmp2 = matrix[idx0 + 6];
@@ -183,24 +269,50 @@ define( function( require ) {
       matrix[idx1 + 6] = -tmp2;
     },
 
+    /*
+     * Sets the result matrix to the identity.
+     *
+     * @param {FastMath.Array} result - [output] 3x3 Matrix
+     */
     setIdentity3: function( result ) {
       result[0] = result[4] = result[8] = 1; // diagonal
       result[1] = result[2] = result[3] = result[5] = result[6] = result[7] = 0; // non-diagonal
     },
 
-    setGivens3: function( result, cos, sin, idx1, idx2 ) {
-      assert && assert( idx1 < idx2 );
+    /*
+     * Sets the result matrix to the Givens rotation (performs a rotation between two components). Instead of an angle,
+     * the 'cos' and 'sin' values are passed in directly since we skip the trigonometry almost everywhere we can.
+     *
+     * See http://en.wikipedia.org/wiki/Givens_rotation (note that we use the other sign convention for the sin)
+     *
+     * @param {FastMath.Array} result - [output] 3x3 Matrix
+     * @param {number} cos - [input] The cosine of the Givens rotation angle
+     * @param {number} sin - [input] The sine of the Givens rotation angle
+     * @param {number} idx0 - [input] The smaller row/column index
+     * @param {number} idx1 - [input] The larger row/column index
+     */
+    setGivens3: function( result, cos, sin, idx0, idx1 ) {
+      assert && assert( idx0 < idx1 );
       this.setIdentity3( result );
+      result[this.index3(idx0,idx0)] = cos;
       result[this.index3(idx1,idx1)] = cos;
-      result[this.index3(idx2,idx2)] = cos;
-      result[this.index3(idx1,idx2)] = sin;
-      result[this.index3(idx2,idx1)] = -sin;
+      result[this.index3(idx0,idx1)] = sin;
+      result[this.index3(idx1,idx0)] = -sin;
     },
 
-    // pre-multiplies the givens rotation in-place (optimized)
-    preMult3Givens: function( matrix, cos, sin, idx1, idx2 ) {
-      var baseA = idx1 * 3;
-      var baseB = idx2 * 3;
+    /*
+     * Efficiently pre-multiples the matrix in-place by the specified Givens rotation (matrix <= rotation * matrix).
+     * Equivalent to using setGivens3 and mult3.
+     *
+     * @param {FastMath.Array} result - [input AND output] 3x3 Matrix
+     * @param {number} cos - [input] The cosine of the Givens rotation angle
+     * @param {number} sin - [input] The sine of the Givens rotation angle
+     * @param {number} idx0 - [input] The smaller row/column index
+     * @param {number} idx1 - [input] The larger row/column index
+     */
+    preMult3Givens: function( matrix, cos, sin, idx0, idx1 ) {
+      var baseA = idx0 * 3;
+      var baseB = idx1 * 3;
       // lexicographically in column-major order for "affine" section
       var a = cos * matrix[baseA + 0] + sin * matrix[baseB + 0];
       var b = cos * matrix[baseB + 0] - sin * matrix[baseA + 0];
@@ -216,27 +328,37 @@ define( function( require ) {
       matrix[baseB + 2] = f;
     },
 
-    // post-multiplies the transpose of the givens rotation in-place (optimized)
-    postMult3Givens: function( matrix, cos, sin, idx1, idx2 ) {
+    /*
+     * Efficiently post-multiples the matrix in-place by the transpose of the specified Givens rotation
+     * (matrix <= matrix * rotation^T).
+     * Equivalent to using setGivens3 and mult3RightTranspose.
+     *
+     * @param {FastMath.Array} result - [input AND output] 3x3 Matrix
+     * @param {number} cos - [input] The cosine of the Givens rotation angle
+     * @param {number} sin - [input] The sine of the Givens rotation angle
+     * @param {number} idx0 - [input] The smaller row/column index
+     * @param {number} idx1 - [input] The larger row/column index
+     */
+    postMult3Givens: function( matrix, cos, sin, idx0, idx1 ) {
       // lexicographically in row-major order for the "transposed affine" section
-      var a = cos * matrix[idx1 + 0] + sin * matrix[idx2 + 0];
-      var b = cos * matrix[idx2 + 0] - sin * matrix[idx1 + 0];
-      var c = cos * matrix[idx1 + 3] + sin * matrix[idx2 + 3];
-      var d = cos * matrix[idx2 + 3] - sin * matrix[idx1 + 3];
-      var e = cos * matrix[idx1 + 6] + sin * matrix[idx2 + 6];
-      var f = cos * matrix[idx2 + 6] - sin * matrix[idx1 + 6];
-      matrix[idx1 + 0] = a;
-      matrix[idx2 + 0] = b;
-      matrix[idx1 + 3] = c;
-      matrix[idx2 + 3] = d;
-      matrix[idx1 + 6] = e;
-      matrix[idx2 + 6] = f;
+      var a = cos * matrix[idx0 + 0] + sin * matrix[idx1 + 0];
+      var b = cos * matrix[idx1 + 0] - sin * matrix[idx0 + 0];
+      var c = cos * matrix[idx0 + 3] + sin * matrix[idx1 + 3];
+      var d = cos * matrix[idx1 + 3] - sin * matrix[idx0 + 3];
+      var e = cos * matrix[idx0 + 6] + sin * matrix[idx1 + 6];
+      var f = cos * matrix[idx1 + 6] - sin * matrix[idx0 + 6];
+      matrix[idx0 + 0] = a;
+      matrix[idx1 + 0] = b;
+      matrix[idx0 + 3] = c;
+      matrix[idx1 + 3] = d;
+      matrix[idx0 + 6] = e;
+      matrix[idx1 + 6] = f;
     },
 
-    applyJacobi3: function( curS, curQ, idx1, idx2 ) {
-      var a11 = curS[3 * idx1 + idx1];
-      var a12 = curS[3 * idx1 + idx2];
-      var a22 = curS[3 * idx2 + idx2];
+    applyJacobi3: function( curS, curQ, idx0, idx1 ) {
+      var a11 = curS[3 * idx0 + idx0];
+      var a12 = curS[3 * idx0 + idx1];
+      var a22 = curS[3 * idx1 + idx1];
 
       // approximate givens angle
       var lhs = a12 * a12;
@@ -249,11 +371,11 @@ define( function( require ) {
       var sin = useAngle ? ( w * a12 ) : SQRT_HALF;
 
       // S' = Q * S * transpose( Q )
-      this.preMult3Givens( curS, cos, sin, idx1, idx2 );
-      this.postMult3Givens( curS, cos, sin, idx1, idx2 );
+      this.preMult3Givens( curS, cos, sin, idx0, idx1 );
+      this.postMult3Givens( curS, cos, sin, idx0, idx1 );
 
       // Q' = Q * curQ
-      this.preMult3Givens( curQ, cos, sin, idx1, idx2 );
+      this.preMult3Givens( curQ, cos, sin, idx0, idx1 );
     },
 
     // TODO: if necessary, hand-code the application of the givens for each index pair
