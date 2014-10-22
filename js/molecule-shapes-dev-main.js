723d44fe5c3d303a3f0ee9592d400b0c45dbcc11
Jonathan Olson <olsonsjc@gmail.com>
2014-10-22 00:05:46 -0600

    Updating dev-main

--- js/molecule-shapes-dev-main.js
+++ js/molecule-shapes-dev-main.js
@@ -9,35 +9,58 @@ define( function( require ) {
   'use strict';
 
   return {
-    // TODO: update
-    RealAtomLocation: require( 'MOLECULE_SHAPES/common/model/RealAtomLocation' ),
+    LiberationSansRegularSubset: require( 'MOLECULE_SHAPES/common/data/LiberationSansRegularSubset' ),
+    LonePairGeometryData: require( 'MOLECULE_SHAPES/common/data/LonePairGeometryData' ),
+
     AttractorModel: require( 'MOLECULE_SHAPES/common/model/AttractorModel' ),
     Bond: require( 'MOLECULE_SHAPES/common/model/Bond' ),
     FastMath: require( 'MOLECULE_SHAPES/common/model/FastMath' ),
     GeometryConfiguration: require( 'MOLECULE_SHAPES/common/model/GeometryConfiguration' ),
     LocalShape: require( 'MOLECULE_SHAPES/common/model/LocalShape' ),
     Molecule: require( 'MOLECULE_SHAPES/common/model/Molecule' ),
+    MoleculeShapesModel: require( 'MOLECULE_SHAPES/common/model/MoleculeShapesModel' ),
     PairGroup: require( 'MOLECULE_SHAPES/common/model/PairGroup' ),
+    RealAtomLocation: require( 'MOLECULE_SHAPES/common/model/RealAtomLocation' ),
     RealMolecule: require( 'MOLECULE_SHAPES/common/model/RealMolecule' ),
     RealMoleculeShape: require( 'MOLECULE_SHAPES/common/model/RealMoleculeShape' ),
     VseprConfiguration: require( 'MOLECULE_SHAPES/common/model/VseprConfiguration' ),
     VSEPRMolecule: require( 'MOLECULE_SHAPES/common/model/VSEPRMolecule' ),
 
+    AtomView: require( 'MOLECULE_SHAPES/common/view/3d/AtomView' ),
+    BondAngleFallbackView: require( 'MOLECULE_SHAPES/common/view/3d/BondAngleFallbackView' ),
+    BondAngleView: require( 'MOLECULE_SHAPES/common/view/3d/BondAngleView' ),
+    BondAngleWebGLView: require( 'MOLECULE_SHAPES/common/view/3d/BondAngleWebGLView' ),
+    BondView: require( 'MOLECULE_SHAPES/common/view/3d/BondView' ),
+    ElectronView: require( 'MOLECULE_SHAPES/common/view/3d/ElectronView' ),
+    LabelWebGLView: require( 'MOLECULE_SHAPES/common/view/3d/LabelWebGLView' ),
+    LocalGeometry: require( 'MOLECULE_SHAPES/common/view/3d/LocalGeometry' ),
+    LocalMaterial: require( 'MOLECULE_SHAPES/common/view/3d/LocalMaterial' ),
+    LocalPool: require( 'MOLECULE_SHAPES/common/view/3d/LocalPool' ),
+    LocalTexture: require( 'MOLECULE_SHAPES/common/view/3d/LocalTexture' ),
+    LonePairView: require( 'MOLECULE_SHAPES/common/view/3d/LonePairView' ),
+    MoleculeView: require( 'MOLECULE_SHAPES/common/view/3d/MoleculeView' ),
+
+    GeometryNamePanel: require( 'MOLECULE_SHAPES/common/view/GeometryNamePanel' ),
+    GlobalOptionsNode: require( 'MOLECULE_SHAPES/common/view/GlobalOptionsNode' ),
+    LabelFallbackNode: require( 'MOLECULE_SHAPES/common/view/LabelFallbackNode' ),
+    MoleculeShapesCheckBox: require( 'MOLECULE_SHAPES/common/view/MoleculeShapesCheckBox' ),
+    MoleculeShapesColors: require( 'MOLECULE_SHAPES/common/view/MoleculeShapesColors' ),
+    MoleculeShapesPanel: require( 'MOLECULE_SHAPES/common/view/MoleculeShapesPanel' ),
+    MoleculeShapesScreenView: require( 'MOLECULE_SHAPES/common/view/MoleculeShapesScreenView' ),
+    OptionsNode: require( 'MOLECULE_SHAPES/common/view/OptionsNode' ),
+    TitledPanel: require( 'MOLECULE_SHAPES/common/view/TitledPanel' ),
+
+    MoleculeShapesGlobals: require( 'MOLECULE_SHAPES/common/MoleculeShapesGlobals' ),
+
+    BondGroupNode: require( 'MOLECULE_SHAPES/model/BondGroupNode' ),
     ModelMoleculesModel: require( 'MOLECULE_SHAPES/model/ModelMoleculesModel' ),
     ModelMoleculesScreen: require( 'MOLECULE_SHAPES/model/ModelMoleculesScreen' ),
     ModelMoleculesScreenView: require( 'MOLECULE_SHAPES/model/ModelMoleculesScreenView' ),
+    RemovePairGroupButton: require( 'MOLECULE_SHAPES/model/RemovePairGroupButton' ),
 
     RealMoleculesModel: require( 'MOLECULE_SHAPES/real/RealMoleculesModel' ),
     RealMoleculesScreen: require( 'MOLECULE_SHAPES/real/RealMoleculesScreen' ),
     RealMoleculesScreenView: require( 'MOLECULE_SHAPES/real/RealMoleculesScreenView' ),
-
-
-    AtomView: require( 'MOLECULE_SHAPES/common/view/3d/AtomView' ),
-    BondView: require( 'MOLECULE_SHAPES/common/view/3d/BondView' ),
-    MoleculeView: require( 'MOLECULE_SHAPES/common/view/3d/MoleculeView' ),
-
-    MoleculeShapesColors: require( 'MOLECULE_SHAPES/common/view/MoleculeShapesColors' ),
-    MoleculeShapesScreenView: require( 'MOLECULE_SHAPES/common/view/MoleculeShapesScreenView' ),
 
     // externals used for debugging
     Element: require( 'NITROGLYCERIN/Element' )
