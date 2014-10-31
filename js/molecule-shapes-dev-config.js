053c5769457e620eec9d2141cf831682bb5785bd
Jonathan Olson <olsonsjc@gmail.com>
2014-10-31 17:08:13 -0600

    Improving config formatting

--- js/molecule-shapes-dev-config.js
+++ js/molecule-shapes-dev-config.js
@@ -10,44 +10,43 @@
 // and script errors
 window.loadedMoleculeShapesConfig = true;
 
-require.config(
-  {
-    // An array of dependencies to load. Useful when require is defined as a config object before require.js
-    // is loaded, and you want to specify dependencies to load as soon as require() is defined.
-    deps: ['molecule-shapes-dev-main'],
+require.config( {
+  // An array of dependencies to load. Useful when require is defined as a config object before require.js
+  // is loaded, and you want to specify dependencies to load as soon as require() is defined.
+  deps: ['molecule-shapes-dev-main'],
 
-    // baseUrl: don't bother trying to set it here, it is overridden by data-main in the top-level HTML file
+  // baseUrl: don't bother trying to set it here, it is overridden by data-main in the top-level HTML file
 
-    // Path mappings for module names not found directly under baseUrl. The path settings are assumed to be
-    // relative to baseUrl unless the paths setting starts with a '/' or has a URL protocol.
-    paths: {
+  // Path mappings for module names not found directly under baseUrl. The path settings are assumed to be
+  // relative to baseUrl unless the paths setting starts with a '/' or has a URL protocol.
+  paths: {
 
-      // third-party libs
-      text: '../../sherpa/text',
+    // third-party libs
+    text: '../../sherpa/text',
 
-      // PhET plugins
-      image: '../../chipper/requirejs-plugins/image',
-      string: '../../chipper/requirejs-plugins/string',
+    // PhET plugins
+    image: '../../chipper/requirejs-plugins/image',
+    string: '../../chipper/requirejs-plugins/string',
 
-      // common directories, uppercase names to identify them in require imports
-      ASSERT: '../../assert/js',
-      AXON: '../../axon/js',
-      BRAND: '../../brand/js',
-      DOT: '../../dot/js',
-      JOIST: '../../joist/js',
-      KITE: '../../kite/js',
-      PHET_CORE: '../../phet-core/js',
-      PHETCOMMON: '../../phetcommon/js',
-      SCENERY: '../../scenery/js',
-      SCENERY_PHET: '../../scenery-phet/js',
-      SUN: '../../sun/js',
+    // common directories, uppercase names to identify them in require imports
+    ASSERT: '../../assert/js',
+    AXON: '../../axon/js',
+    BRAND: '../../brand/js',
+    DOT: '../../dot/js',
+    JOIST: '../../joist/js',
+    KITE: '../../kite/js',
+    PHET_CORE: '../../phet-core/js',
+    PHETCOMMON: '../../phetcommon/js',
+    SCENERY: '../../scenery/js',
+    SCENERY_PHET: '../../scenery-phet/js',
+    SUN: '../../sun/js',
 
-      // extra dependencies
-      NITROGLYCERIN: '../../nitroglycerin/js',
+    // extra dependencies
+    NITROGLYCERIN: '../../nitroglycerin/js',
 
-      // this sim
-      MOLECULE_SHAPES: '.'
-    },
+    // this sim
+    MOLECULE_SHAPES: '.'
+  },
 
-    urlArgs: new Date().getTime()  // cache buster to make browser reload all included scripts
-  } );
+  urlArgs: new Date().getTime()  // cache buster to make browser reload all included scripts
+} );
