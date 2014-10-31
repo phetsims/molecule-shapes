ba551a6adc8ddb4885d39d898fdd96b6480724e9
Jonathan Olson <olsonsjc@gmail.com>
2014-10-31 17:09:44 -0600

    Font embedding reason doc, see #60

--- js/common/data/LiberationSansRegularSubset.js
+++ js/common/data/LiberationSansRegularSubset.js
@@ -1,7 +1,8 @@
 // Copyright 2002-2014, University of Colorado Boulder
 
 /**
- * Numeric subset of Liberation Sans Regular
+ * Numeric subset of Liberation Sans Regular, used for dynamic text performance (changing 12+ text values per frame was
+ * a major bottleneck when using SVG text).
  * (uses the SIL Open Font License, http://en.wikipedia.org/wiki/SIL_Open_Font_License)
  *
  * Extracted paths and x-advances for the glyphs using FreeType.
