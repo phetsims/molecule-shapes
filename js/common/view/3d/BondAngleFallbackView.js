9884869a9e1fdf534f3bce1bce5e41b0c864ff2b
Jonathan Olson <olsonsjc@gmail.com>
2014-12-04 11:13:11 -0700

    Removing stored references to our color link listener, fixes #91

--- js/common/view/3d/BondAngleFallbackView.js
+++ js/common/view/3d/BondAngleFallbackView.js
@@ -73,13 +73,13 @@ define( function( require ) {
       depthWrite: false, // don't write depth values, so we don't cause other transparent objects to render
       overdraw: MoleculeShapesGlobals.useWebGL ? 0 : 0.1 // amount to extend polygons when using Canvas to avoid cracks
     } );
-    this.unlinkSectorColor = MoleculeShapesGlobals.linkColor( this.sectorMaterial, MoleculeShapesColors.bondAngleSweepProperty );
+    MoleculeShapesGlobals.linkColor( this.sectorMaterial, MoleculeShapesColors.bondAngleSweepProperty );
     this.arcMaterial = new THREE.LineBasicMaterial( {
       transparent: true,
       opacity: 0.7,
       depthWrite: false // don't write depth values, so we don't cause other transparent objects to render
     } );
-    this.unlinkArcColor = MoleculeShapesGlobals.linkColor( this.arcMaterial, MoleculeShapesColors.bondAngleArcProperty );
+    MoleculeShapesGlobals.linkColor( this.arcMaterial, MoleculeShapesColors.bondAngleArcProperty );
 
     this.sectorView = new THREE.Mesh( this.sectorGeometry, this.sectorMaterial );
     this.arcView = new THREE.Line( this.arcGeometry, this.arcMaterial );
