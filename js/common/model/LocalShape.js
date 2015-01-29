ed9fbd2d9bee108bb1da4003620e3de91894c2c0
Jonathan Olson <olsonsjc@gmail.com>
2015-01-28 22:24:48 -0700

    Moving static functions to inherit call, see #75

--- js/common/model/LocalShape.js
+++ js/common/model/LocalShape.js
@@ -53,85 +53,85 @@ define( function( require ) {
     applyAngleAttractionRepulsion: function( dt ) {
       AttractorModel.applyAttractorForces( this.groups, dt, this.idealOrientations, this.allowedPermutations, this.centralAtom.position, true );
     }
-  } );
+  }, {
+    /**
+     * Given a list of permutations, return all permutations that exist with the specified indices permuted in all different ways.
+     * <p/>
+     * IE, if given the list of the single permutation (12), and specified indices {3,4,5}, the permutations returned will be
+     * (12)(34),(12)(35),(12)(45),(12)(453),(12)(534),(12)
+     */
+    permuteListWithIndices: function( permutations, indices ) {
+      if ( indices.length < 2 ) {
+        // no changes if we can't move more than 1 element (need somewhere to put it)
+        return permutations;
+      }
+      var result = [];
+      for ( var i = 0; i < permutations.length; i++ ) {
+        var permutation = permutations[ i ];
 
-  /**
-   * Given a list of permutations, return all permutations that exist with the specified indices permuted in all different ways.
-   * <p/>
-   * IE, if given the list of the single permutation (12), and specified indices {3,4,5}, the permutations returned will be
-   * (12)(34),(12)(35),(12)(45),(12)(453),(12)(534),(12)
-   */
-  LocalShape.permuteListWithIndices = function( permutations, indices ) {
-    if ( indices.length < 2 ) {
-      // no changes if we can't move more than 1 element (need somewhere to put it)
+        var resultsToAdd = permutation.withIndicesPermuted( indices );
+        for ( var j = 0; j < resultsToAdd.length; j++ ) {
+          result.push( resultsToAdd[ j ] );
+        }
+      }
+      return result;
+    },
+
+    // allow switching of lone pairs with each other, and all other types of bonds with each other
+    // NOTE: I recommended double or triple bonds being put in "higher repulsion" spots over single bonds, but this was specifically rejected. -JO
+    vseprPermutations: function( neighbors ) {
+      var permutations = [];
+      permutations.push( Permutation.identity( neighbors.length ) );
+
+      var indexOf = function( group ) {
+        return neighbors.indexOf( group );
+      };
+
+      // partition the neighbors into lone pairs and atoms.
+      var partitioned = partition( neighbors, function( group ) {
+        return group.isLonePair;
+      } );
+      // this separation looks better in languages where you say "(lonePairs, atoms) = partition(...)"
+      var lonePairs = partitioned[ 0 ];
+      var atoms = partitioned[ 1 ];
+
+      // permute away the lone pairs
+      permutations = LocalShape.permuteListWithIndices( permutations, _.map( lonePairs, indexOf ) );
+
+      // permute away the bonded groups
+      permutations = LocalShape.permuteListWithIndices( permutations, _.map( atoms, indexOf ) );
+      return permutations;
+    },
+
+    // allow switching of lone pairs with each other, and all other types of bonds with the same type of element
+    realPermutations: function( neighbors ) {
+      var permutations = [];
+      permutations.push( Permutation.identity( neighbors.length ) );
+
+      var indexOf = function( group ) {
+        return neighbors.indexOf( group );
+      };
+
+      // allow interchanging of lone pairs
+      var lonePairs = _.filter( neighbors, function( group ) { return group.isLonePair; } );
+      permutations = LocalShape.permuteListWithIndices( permutations, _.map( lonePairs, indexOf ) );
+
+      // allow interchanging of pair groups when they have the same chemical element
+      var atoms = _.filter( neighbors, function( group ) { return !group.isLonePair; } );
+
+      var usedElements = _.unique( _.map( atoms, function( group ) { return group.element; } ) );
+
+      for ( var i = 0; i < usedElements.length; i++ ) {
+        var element = usedElements[ i ];
+
+        // since the closure is being executed at this point, the warning in this line can be ignored
+        var atomsWithElement = _.filter( atoms, function( group ) { return group.element === element; } );
+        permutations = LocalShape.permuteListWithIndices( permutations, _.map( atomsWithElement, indexOf ) );
+      }
+
       return permutations;
     }
-    var result = [];
-    for ( var i = 0; i < permutations.length; i++ ) {
-      var permutation = permutations[ i ];
-
-      var resultsToAdd = permutation.withIndicesPermuted( indices );
-      for ( var j = 0; j < resultsToAdd.length; j++ ) {
-        result.push( resultsToAdd[ j ] );
-      }
-    }
-    return result;
-  };
-
-  // allow switching of lone pairs with each other, and all other types of bonds with each other
-  // NOTE: I recommended double or triple bonds being put in "higher repulsion" spots over single bonds, but this was specifically rejected. -JO
-  LocalShape.vseprPermutations = function( neighbors ) {
-    var permutations = [];
-    permutations.push( Permutation.identity( neighbors.length ) );
-
-    var indexOf = function( group ) {
-      return neighbors.indexOf( group );
-    };
-
-    // partition the neighbors into lone pairs and atoms.
-    var partitioned = partition( neighbors, function( group ) {
-      return group.isLonePair;
-    } );
-    // this separation looks better in languages where you say "(lonePairs, atoms) = partition(...)"
-    var lonePairs = partitioned[ 0 ];
-    var atoms = partitioned[ 1 ];
-
-    // permute away the lone pairs
-    permutations = LocalShape.permuteListWithIndices( permutations, _.map( lonePairs, indexOf ) );
-
-    // permute away the bonded groups
-    permutations = LocalShape.permuteListWithIndices( permutations, _.map( atoms, indexOf ) );
-    return permutations;
-  };
-
-  // allow switching of lone pairs with each other, and all other types of bonds with the same type of element
-  LocalShape.realPermutations = function( neighbors ) {
-    var permutations = [];
-    permutations.push( Permutation.identity( neighbors.length ) );
-
-    var indexOf = function( group ) {
-      return neighbors.indexOf( group );
-    };
-
-    // allow interchanging of lone pairs
-    var lonePairs = _.filter( neighbors, function( group ) { return group.isLonePair; } );
-    permutations = LocalShape.permuteListWithIndices( permutations, _.map( lonePairs, indexOf ) );
-
-    // allow interchanging of pair groups when they have the same chemical element
-    var atoms = _.filter( neighbors, function( group ) { return !group.isLonePair; } );
-
-    var usedElements = _.unique( _.map( atoms, function( group ) { return group.element; } ) );
-
-    for ( var i = 0; i < usedElements.length; i++ ) {
-      var element = usedElements[ i ];
-
-      // since the closure is being executed at this point, the warning in this line can be ignored
-      var atomsWithElement = _.filter( atoms, function( group ) { return group.element === element; } );
-      permutations = LocalShape.permuteListWithIndices( permutations, _.map( atomsWithElement, indexOf ) );
-    }
-
-    return permutations;
-  };
+  } );
 
   return LocalShape;
 } );
