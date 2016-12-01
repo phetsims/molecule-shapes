543d094f1dfb3e95ff27ce7f8d74c18ca90f6f14
zepumph <michael.kauzmann@colorado.edu>
2016-12-01 15:24:40 -0700

    Added copyright statement for lint see https://github.com/phetsims/special-ops/issues/49

--- tests/qunit/unit-tests.js
+++ tests/qunit/unit-tests.js
@@ -1,3 +1,5 @@
+// Copyright 2016, University of Colorado Boulder
+
 // path to test base, without a slash, e.g. '../../common/scenery/tests/qunit'
 function runMoleculeShapesTests( pathToTestBase ) {
   function loadTestFile( src ) {
