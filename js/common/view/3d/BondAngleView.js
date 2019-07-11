e66b80bd2df4f6c8a9d90de6cffffda0bb6d97b2
zepumph <michael.kauzmann@colorado.edu>
2019-07-11 12:54:09 -0800

    update copyright dates, https://github.com/phetsims/tasks/issues/988

--- js/common/view/3d/BondAngleView.js
+++ js/common/view/3d/BondAngleView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2019, University of Colorado Boulder
 
 /**
  * Base type for views of the angle (sector and line) between two bonds (not including the displayed label)
--- js/common/view/3d/LabelWebGLView.js
+++ js/common/view/3d/LabelWebGLView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2019, University of Colorado Boulder
 
 /**
  * Label (text) of the angle between two bonds. We restrict the possible input strings to the form '123.4°' or '12.3°'
