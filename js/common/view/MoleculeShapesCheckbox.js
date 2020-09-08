e8c5d3c7dac41e8ab5e57a590b057caf53ea1624
pixelzoom <cmalley@pixelzoom.com>
2020-09-08 16:19:51 -0600

    convert Checkbox to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/common/view/MoleculeShapesCheckbox.js
+++ js/common/view/MoleculeShapesCheckbox.js
@@ -6,24 +6,24 @@
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import Checkbox from '../../../../sun/js/Checkbox.js';
 import moleculeShapes from '../../moleculeShapes.js';
 import MoleculeShapesColorProfile from './MoleculeShapesColorProfile.js';
 
-/**
- * @param {Node} content
- * @param {Property.<boolean>} property
- * @constructor
- * @param {Object} [options]
- */
-function MoleculeShapesCheckbox( content, property, options ) {
-  Checkbox.call( this, content, property, options );
+class MoleculeShapesCheckbox extends Checkbox {
 
-  MoleculeShapesColorProfile.checkboxProperty.linkAttribute( this, 'checkboxColor' );
-  MoleculeShapesColorProfile.checkboxBackgroundProperty.linkAttribute( this, 'checkboxColorBackground' );
+  /**
+   * @param {Node} content
+   * @param {Property.<boolean>} property
+   * @param {Object} [options]
+   */
+  constructor( content, property, options ) {
+    super( content, property, options );
+
+    MoleculeShapesColorProfile.checkboxProperty.linkAttribute( this, 'checkboxColor' );
+    MoleculeShapesColorProfile.checkboxBackgroundProperty.linkAttribute( this, 'checkboxColorBackground' );
+  }
 }
 
 moleculeShapes.register( 'MoleculeShapesCheckbox', MoleculeShapesCheckbox );
-inherit( Checkbox, MoleculeShapesCheckbox );
 export default MoleculeShapesCheckbox;
