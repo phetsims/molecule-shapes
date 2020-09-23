dd605d051ffa82932d36d703ab44374eedfd6006
pixelzoom <cmalley@pixelzoom.com>
2020-09-22 18:50:04 -0600

    convert Atom and subclasses to ES6, https://github.com/phetsims/tasks/issues/1044

--- js/common/model/RealAtomPosition.js
+++ js/common/model/RealAtomPosition.js
@@ -7,29 +7,28 @@
  */
 
 import Atom from '../../../../nitroglycerin/js/Atom.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import moleculeShapes from '../../moleculeShapes.js';
 
-/**
- * @constructor
- * @param {Element} element
- * @param {Vector2} position - The initial position for the atom
- * @param {number} [lonePairCount]
- */
-function RealAtomPosition(element, position, lonePairCount ) {
-  Atom.call( this, element );
+class RealAtomPosition extends Atom {
 
-  this.position = position; // @public {Vector3}
-  this.orientation = position.magnitude > 0 ? position.normalized() : position.copy(); // @public {Vector3}
-  this.lonePairCount = lonePairCount || 0; // @public {number} - How many external lone pairs it has.
+  /**
+   * @param {Element} element
+   * @param {Vector2} position - The initial position for the atom
+   * @param {number} [lonePairCount]
+   */
+  constructor( element, position, lonePairCount ) {
+    super( element );
 
-  // we should be immutable
-  if ( assert ) {
-    Object.freeze && Object.freeze( this );
+    this.position = position; // @public {Vector3}
+    this.orientation = position.magnitude > 0 ? position.normalized() : position.copy(); // @public {Vector3}
+    this.lonePairCount = lonePairCount || 0; // @public {number} - How many external lone pairs it has.
+
+    // we should be immutable
+    if ( assert ) {
+      Object.freeze && Object.freeze( this );
+    }
   }
 }
 
 moleculeShapes.register( 'RealAtomPosition', RealAtomPosition );
-
-inherit( Atom, RealAtomPosition );
 export default RealAtomPosition;
