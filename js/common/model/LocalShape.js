fa0ea6e7add0490490245e9eda5074e638455834
phet-dev <phetadmin@gmail.com>
2022-01-20 03:24:20 -0700

    update copyright dates from daily grunt work

--- js/common/model/LocalShape.js
+++ js/common/model/LocalShape.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2021, University of Colorado Boulder
+// Copyright 2013-2022, University of Colorado Boulder
 
 /**
  * The ideal local shape for a certain central atom and its (local) neighbors.
