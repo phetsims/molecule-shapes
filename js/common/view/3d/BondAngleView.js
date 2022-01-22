c7d39ad9ca22d87c1529cb0f705e201a463c981e
phet-dev <phetadmin@gmail.com>
2022-01-22 03:22:36 -0700

    update copyright dates from daily grunt work

--- js/common/view/3d/BondAngleView.js
+++ js/common/view/3d/BondAngleView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * Base type for views of the angle (sector and line) between two bonds (not including the displayed label)
--- js/common/view/GeometryNamePanel.js
+++ js/common/view/GeometryNamePanel.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2021, University of Colorado Boulder
+// Copyright 2014-2022, University of Colorado Boulder
 
 /**
  * Shows the molecular and electron geometry names, and has checkboxes which allow toggling their visibility
