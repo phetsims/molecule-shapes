d5596ba61ebca1725c8016c57ffdb76977360c8d
zepumph <michael.kauzmann@colorado.edu>
2022-03-04 13:08:22 -0700

    import kite files from imports.ts, https://github.com/phetsims/kite/issues/96

--- js/common/view/3d/LabelWebGLView.js
+++ js/common/view/3d/LabelWebGLView.js
@@ -16,7 +16,7 @@
 import Bounds2 from '../../../../../dot/js/Bounds2.js';
 import Matrix3 from '../../../../../dot/js/Matrix3.js';
 import Vector2 from '../../../../../dot/js/Vector2.js';
-import Shape from '../../../../../kite/js/Shape.js';
+import { Shape } from '../../../../../kite/js/imports.js';
 import { Utils } from '../../../../../scenery/js/imports.js';
 import moleculeShapes from '../../../moleculeShapes.js';
 import MoleculeShapesGlobals from '../../MoleculeShapesGlobals.js';
