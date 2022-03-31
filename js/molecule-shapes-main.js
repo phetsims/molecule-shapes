c50909b4fa0e55dca00e5bf3173d4496474768e4
Jonathan Olson <jonathan.olson@colorado.edu>
2022-03-30 18:53:39 -0600

    Removing IE11StencilWarningNode.js, see https://github.com/phetsims/molecule-shapes/issues/217

--- js/molecule-shapes-main.js
+++ js/molecule-shapes-main.js
@@ -9,7 +9,6 @@
 import Sim from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import CanvasWarningNode from '../../scenery-phet/js/CanvasWarningNode.js';
-import IE11StencilWarningNode from './common/view/IE11StencilWarningNode.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import MoleculeShapesGlobals from './common/MoleculeShapesGlobals.js';
 import GlobalOptionsNode from './common/view/GlobalOptionsNode.js';
@@ -33,11 +32,7 @@ const simOptions = {
   // Creates content for the Options dialog
   createOptionsDialogContent: tandem => new GlobalOptionsNode( isBasicsVersion, tandem ),
 
-  homeScreenWarningNode: MoleculeShapesGlobals.useWebGLProperty.value ?
-                         null :
-                         ( MoleculeShapesGlobals.hasBasicWebGLSupportProperty.value ?
-                           new IE11StencilWarningNode() : // if we have basic support, we failed due to IE-specific reasons
-                           new CanvasWarningNode() )
+  homeScreenWarningNode: MoleculeShapesGlobals.useWebGLProperty.value ? null : new CanvasWarningNode()
 };
 
 // NOTE: ?webgl=false will trigger Canvas rendering with a reduced poly-count
