c49a83640213e98366ca5ea51b076c7d9d707902
phet-dev <phetadmin@gmail.com>
2023-04-05 04:55:35 -0600

    update copyright dates from daily grunt work

--- js/common/model/ElectronGeometry.js
+++ js/common/model/ElectronGeometry.js
@@ -1,4 +1,4 @@
-// Copyright 2013-2022, University of Colorado Boulder
+// Copyright 2013-2023, University of Colorado Boulder
 
 /**
  * Contains the "optimal" molecule structures (pair group directions stored as unit vectors),
