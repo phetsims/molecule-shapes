393f41bd2c3c0a8658d24502df241522aec9e0b7
phet-dev <phetadmin@gmail.com>
2023-08-16 03:34:23 -0600

    update copyright dates from daily grunt work

--- js/common/view/3d/LocalMaterial.js
+++ js/common/view/3d/LocalMaterial.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * Provides access to renderer-specific materials which are otherwise identical. We can't share materials across
