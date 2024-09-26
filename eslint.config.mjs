b011b84c2f11673c404f114e7768896209659fb4
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:10:00 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for molecule-shapes.
+ * ESLint configuration for molecule-shapes.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
