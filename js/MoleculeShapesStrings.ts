a605edac2db683b874d1a9bdbdd4d7c25de650cc
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-01-27 12:39:34 -0700

    add "supportsDynamicLocale": true to sims that support it but didn't have it, https://github.com/phetsims/joist/issues/1005

--- js/MoleculeShapesStrings.ts
+++ js/MoleculeShapesStrings.ts
@@ -13,85 +13,50 @@ import moleculeShapes from './moleculeShapes.js';
 
 type StringsType = {
   'molecule-shapes': {
-    'title': string;
     'titleStringProperty': LocalizedStringProperty;
   };
   'screen': {
-    'model': string;
     'modelStringProperty': LocalizedStringProperty;
-    'realMolecules': string;
     'realMoleculesStringProperty': LocalizedStringProperty;
   };
   'geometry': {
-    'diatomic': string;
     'diatomicStringProperty': LocalizedStringProperty;
-    'linear': string;
     'linearStringProperty': LocalizedStringProperty;
-    'trigonalPlanar': string;
     'trigonalPlanarStringProperty': LocalizedStringProperty;
-    'tetrahedral': string;
     'tetrahedralStringProperty': LocalizedStringProperty;
-    'trigonalBipyramidal': string;
     'trigonalBipyramidalStringProperty': LocalizedStringProperty;
-    'octahedral': string;
     'octahedralStringProperty': LocalizedStringProperty;
   };
   'shape': {
-    'diatomic': string;
     'diatomicStringProperty': LocalizedStringProperty;
-    'linear': string;
     'linearStringProperty': LocalizedStringProperty;
-    'bent': string;
     'bentStringProperty': LocalizedStringProperty;
-    'trigonalPlanar': string;
     'trigonalPlanarStringProperty': LocalizedStringProperty;
-    'trigonalPyramidal': string;
     'trigonalPyramidalStringProperty': LocalizedStringProperty;
-    'tShaped': string;
     'tShapedStringProperty': LocalizedStringProperty;
-    'tetrahedral': string;
     'tetrahedralStringProperty': LocalizedStringProperty;
-    'seesaw': string;
     'seesawStringProperty': LocalizedStringProperty;
-    'squarePlanar': string;
     'squarePlanarStringProperty': LocalizedStringProperty;
-    'trigonalBipyramidal': string;
     'trigonalBipyramidalStringProperty': LocalizedStringProperty;
-    'squarePyramidal': string;
     'squarePyramidalStringProperty': LocalizedStringProperty;
-    'octahedral': string;
     'octahedralStringProperty': LocalizedStringProperty;
   };
   'control': {
-    'bonding': string;
     'bondingStringProperty': LocalizedStringProperty;
-    'lonePair': string;
     'lonePairStringProperty': LocalizedStringProperty;
-    'options': string;
     'optionsStringProperty': LocalizedStringProperty;
-    'geometryName': string;
     'geometryNameStringProperty': LocalizedStringProperty;
-    'moleculeGeometry': string;
     'moleculeGeometryStringProperty': LocalizedStringProperty;
-    'electronGeometry': string;
     'electronGeometryStringProperty': LocalizedStringProperty;
-    'showLonePairs': string;
     'showLonePairsStringProperty': LocalizedStringProperty;
-    'showBondAngles': string;
     'showBondAnglesStringProperty': LocalizedStringProperty;
-    'removeAll': string;
     'removeAllStringProperty': LocalizedStringProperty;
-    'molecule': string;
     'moleculeStringProperty': LocalizedStringProperty;
-    'realView': string;
     'realViewStringProperty': LocalizedStringProperty;
-    'modelView': string;
     'modelViewStringProperty': LocalizedStringProperty;
   };
   'options': {
-    'showOuterLonePairs': string;
     'showOuterLonePairsStringProperty': LocalizedStringProperty;
-    'projectorColors': string;
     'projectorColorsStringProperty': LocalizedStringProperty;
   }
 };
