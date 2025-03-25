e301693ec932453392926fb90e64a18db9bcda4b
Michael Kauzmann <michael.kauzmann@colorado.edu>
2025-03-25 16:44:14 -0600

    runnable *QueryParameters.js files to use QueryStringMachineModule, https://github.com/phetsims/query-string-machine/issues/54

--- js/common/MoleculeShapesQueryParameters.js
+++ js/common/MoleculeShapesQueryParameters.js
@@ -6,6 +6,7 @@
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
+import { QueryStringMachine } from '../../../query-string-machine/js/QueryStringMachineModule.js';
 import moleculeShapes from '../moleculeShapes.js';
 
 const MoleculeShapesQueryParameters = QueryStringMachine.getAll( {
