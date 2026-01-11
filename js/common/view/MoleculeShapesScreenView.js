2ad7d8a615c1bf9b4abe85a05f899aefa3b87fae
phet-dev <phetadmin@gmail.com>
2026-01-11 04:56:47 -0700

    update copyright dates from daily grunt work

--- js/common/view/MoleculeShapesScreenView.js
+++ js/common/view/MoleculeShapesScreenView.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2025, University of Colorado Boulder
+// Copyright 2014-2026, University of Colorado Boulder
 
 /**
  * Base view for all "show a single molecule in the center" screens
